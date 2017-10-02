ffb9e0785d0c11f05741ac668a3084c005a0f637
Stas Bannikov <bsadnu@gmail.com>
2017-10-02 12:58:50 +0300

    Delete authors.sql

--- authors.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `authors` (
-	`id` int(10) unsigned NOT NULL,
-	`first_name` varchar(255) NOT NULL,
-	`last_name` varchar(255) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2766 DEFAULT CHARSET=utf8;
-
-ALTER TABLE `authors`
-	ADD PRIMARY KEY (`id`);
